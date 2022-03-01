5bf9b267a3cf1e7a158148955ef273bd3ed509ae
OSA\00004980 <fulande@osa.com>
2022-03-01 19:21:01 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <el-button type="primary" @click="handleChangeTheme">btn</el-button>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      dialogVisible: true,
+      theme: 'black-theme'
+    }
+  },
+  methods: {
+    handleChangeTheme() {
+      document.body.className = this.theme = this.theme === 'black-theme' ? 'blue-theme' : 'black-theme'
+      this.dialogVisible = !this.dialogVisible
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
