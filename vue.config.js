4ffdb17714b8019e2d099b84a7adebb5a9ae74cc
OSA\00004980 <fulande@osa.com>
2022-03-03 09:19:08 +0800

    feat: update

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,43 @@ module.exports = {
     electronBuilder: {
       nodeIntegration: true,
       contextIsolation: false,
-      customFileProtocol: './'
+      customFileProtocol: './',
+      productName: '谭飞',
+      appId: 'com.example.yourapp',
+      // directories: {
+      //   output: 'build'
+      // },
+      // files: ['dist/electron/**/*'],
+      publish: [
+        {
+          provider: 'yanhaichao',
+          url: 'https://malltest.osa.com/electron/'
+        }
+      ],
+      dmg: {
+        contents: [
+          {
+            x: 410,
+            y: 150,
+            type: 'link',
+            path: '/Applications'
+          },
+          {
+            x: 130,
+            y: 150,
+            type: 'file'
+          }
+        ]
+      },
+      mac: {
+        icon: 'build/icons/icon.icns'
+      },
+      win: {
+        icon: 'build/icons/icon.ico'
+      },
+      linux: {
+        icon: 'build/icons'
+      }
     }
   }
 }
